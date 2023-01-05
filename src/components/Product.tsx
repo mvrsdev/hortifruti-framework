8f99954a1bf91ed5d9e43704904cd8c6004c5184
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-05 19:20:15 -0300

    Create product component

---
+++ src/components/Product.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProductContainer = styled.div`
+  align-items: center;
+  height: 'auto';
+  width: 200px;
+  border: 1px solid #f1f1f1;
+  border-radius: 8px;
+  margin: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  &:hover {
+    box-shadow: 0 0 10px 5px #f7f7f7;
+  }
+`;
+
+const InfoContainer = styled.div`
+margin: 8px 0 0 0;
+padding: 1px 12px;
+flex-grow: 1;
+width: 176px;
+background-color: #fafafa;
+border-radius: 0 0 8px 8px;
+color: #444e5e;
+`;
+
+const PriceContainer = styled.p`
+    color: #ff4e4e;
+    font-weight: 600;
+`;
+
+  const AddButton = styled.button`
+    position: relative;
+    top: 40px;
+    right: -64px;
+    border-radius: 100%;
+    width: 50px;
+    height: 50px;
+    border: 1px solid white;
+    background: #ff4e4e;
+    font-size: 36px;
+    color: white;
+    font-weight: lighter;
+    &:hover {
+      filter: brightness(93%)
+    }
+  `;
+
+interface ProductProps {
+  name: string;
+  price: number;
+  image: string;
+  onAddToCart: () => void;
+}
+
+const Product: React.FC<ProductProps> = ({
+  name,
+  price,
+  image,
+  onAddToCart,
+}) => {
+  return (
+    <ProductContainer>
+        <img src={image} alt={name} width={150} height={150}/>
+        <AddButton onClick={() => onAddToCart()}>+</AddButton>
+      <InfoContainer>
+        <h3>{name}</h3>
+        <PriceContainer>${price}</PriceContainer>
+      </InfoContainer>
+    </ProductContainer>
+  );
+};
+
+export default Product;
