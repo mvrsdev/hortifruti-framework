bba15bf4624935ea54b59d981013107db181bdc0
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-07 03:13:24 -0300

    refactor login button comp

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -14,7 +14,7 @@ transition: all 190ms;
   padding: 0 16px;
   cursor: pointer;
   &:hover {
-        border: 1px solid #1a73e8;
+        border: 1px solid #ff4e4e;
         filter: brightness(105%);
         box-shadow: 0 1px 2px 0 rgb(66 133 244 / 30%),
           0 1px 3px 1px rgb(66 133 244 / 15%);
