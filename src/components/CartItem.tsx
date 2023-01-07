4070c7fc2af157b53c90977dc8d9697da053176c
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-07 03:15:21 -0300

    create cart item component

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from './index';
+
+interface CartItemProps {
+  product: {
+    name: string;
+    price: number;
+    image: string;
+  };
+  quantity: number;
+  onRemove: () => void;
+}
+
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 6px;
+  padding: 8px 16px;
+  margin: 16px 10px;
+  justify-content: space-between;
+  background-color: #fff;
+`;
+
+const QuantityContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+`;
+
+const ButtonContainer = styled.button`
+  background: none;
+  border: none;
+  color: #ff4e4e;
+  font-size: 24px;
+  font-weight: lighter;
+  &:hover {
+    filter: brightness(85%);
+  }
+`;
+
+const RemoveButton = styled.button`
+  background: none;
+  border: none;
+  color: #ff4e4e;
+  font-size: 16px;
+  font-weight: bolder;
+  &:hover {
+    filter: brightness(75%);
+  }
+`;
+
+const TotalInput = styled.input`
+  display: inline-flex;
+  font-size: 1rem;
+  max-width: 40px;
+  height: 32px;
+  border-radius: 4px;
+  align-items: center;
+  border: 1px solid #dbdbdb;
+  color: #444e5e;
+  text-align: center;
+  margin: 0 10px;
+`;
+
+const CartItem: React.FC<CartItemProps> = ({ product, quantity }, onRemove) => {
+  return (
+    <ItemContainer>
+      <img src={product.image} alt={product.name} width={60} height={60} />
+      <h4>{product.name}</h4>
+      <p>${product.price}</p>
+      <QuantityContainer>
+        <ButtonContainer onClick={() => null}>-</ButtonContainer>
+        <TotalInput type="text" value={quantity} />
+        <ButtonContainer onClick={() => null}>+</ButtonContainer>
+      </QuantityContainer>
+      <RemoveButton onClick={onRemove}>REMOVE</RemoveButton>
+    </ItemContainer>
+  );
+};
+
+export default CartItem;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
+export { default as CartItem } from './CartItem';
 export { default as Header } from './Header';
 export { default as LoginButton } from './LoginButton';
 export { default as Product } from './Product';
