c106db7ba2c467e4bb96f1be1a0b5c2094dcc608
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-06 23:29:14 -0300

    create ecommerce page comp

---
+++ src/pages/Ecommerce.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header, ProductList } from '../components';
+
+const Ecommerce = () => {
+  return (
+    <>
+      <Header />
+      <ProductList/>
+      
+    </>
+  );
+};
+
+export default Ecommerce;
