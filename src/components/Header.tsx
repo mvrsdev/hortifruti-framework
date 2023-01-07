945571247fd25057efc89c49fe6e14d37a8d5472
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-07 03:35:31 -0300

    refactor header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LoginButton } from './index';
+import shoppingCart from '../assets/img/shopping-cart.png';
 
 const HeaderRow = styled.div`
   display: flex;
   height: 60px;
-  padding: 0 8px;
+  padding: 8px 16px;
   justify-content: space-between;
   color: #000000de;
   align-items: center;
   font-size: 14px;
 `;
 
+const LogoContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 8px;
+cursor: pointer;
+h1 {
+  margin-left: 8px;
+}
+`;
+
 const Header: React.FC = () => {
   return (
-    <>
       <HeaderRow>
+        <LogoContainer>
+        <img src={shoppingCart} alt='Shopping Cart' width={26} height={26}/>
         <h1>Hortifruti Framework</h1>
+        </LogoContainer>
       <LoginButton onClick={() => null}/>
       </HeaderRow>
-    </>
   );
 };
 
