6dfa6e0414c5cb76a26e5e2519c27f352f2e914f
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 04:16:30 -0300

    Create Context

---
+++ src/components/CartContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+
+export interface CartItemProps {
+  product: {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+  };
+  quantity: number;
+}
+
+interface CartContextType {
+  cart: CartItemProps[];
+  setCart: Dispatch<SetStateAction<CartItemProps[]>>;
+}
+
+export const CartContext = createContext<CartContextType>({
+  cart: [],
+  setCart: () => {},
+});
+
+export default CartContext;
