e7bd30d2dd08f19b73a1e3142898261b11d9c39b
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 01:25:48 -0300

    refacton button comp

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,28 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ButtonProps {
-  value: string;
+  width?: number;
+  children: React.ReactNode;
+  onClick: () => void;
 }
 
 const StyledButton = styled.button`
-  position: relative;
-  top: 40px;
-  right: -64px;
-  border-radius: 100%;
-  width: 50px;
-  height: 50px;
-  border: 1px solid white;
-  background: #ff4e4e;
-  font-size: 36px;
-  color: white;
-  font-weight: lighter;
+  transition: all 190ms;
+  min-height: 36px;
+  min-width: 64px;
+  border-radius: 4px;
+  background-color: #ff4e4e;
+  color: #fff;
+  border: 1px solid #ff4e4e;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 0 16px;
+  cursor: pointer;
   &:hover {
-    filter: brightness(93%);
-  }
+        border: 1px solid #ff4e4e;
+        filter: brightness(115%);
+        box-shadow: 0 1px 2px 0 rgb(66 133 244 / 30%),
+          0 1px 3px 1px rgb(66 133 244 / 15%);
+      }
+
+  /* &:active {
+    filter: brightness(85%);
+  } */
 `;
 
-const Button: React.FC<ButtonProps> = ({ value }) => {
-  return <StyledButton>{value}</StyledButton>;
+const Button: React.FC<ButtonProps> = ({ children, onClick, width }) => {
+  return <StyledButton style={{width:width}} onClick={onClick}>{children}</StyledButton>;
 };
 
 export default Button;
