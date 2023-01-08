b0fc3a12319aee219983e5fe30e39128c417be96
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 01:27:37 -0300

    refactor button in login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,40 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const ButtonContainer = styled.button`
-transition: all 190ms;
-  min-height: 36px;
-  min-width: 64px;
-  border-radius: 4px;
-  background-color: #ff4e4e;
-  color: #fff;
-  border: 1px solid #ff4e4e;
-  font-size: 14px;
-  font-weight: 600;
-  padding: 0 16px;
-  cursor: pointer;
-  &:hover {
-        border: 1px solid #ff4e4e;
-        filter: brightness(105%);
-        box-shadow: 0 1px 2px 0 rgb(66 133 244 / 30%),
-          0 1px 3px 1px rgb(66 133 244 / 15%);
-      }
-
-  &:active {
-    filter: brightness(85%);
-  }
-`;
+import { Button } from './index';
 
 interface Props {
   onClick: () => void;
 }
 
 const LoginButton: React.FC<Props> = ({ onClick }) => {
-  return (
-    <ButtonContainer onClick={onClick}>
-      Login
-    </ButtonContainer>
-  );
+  return <Button onClick={onClick}>Login</Button>;
 };
 
 export default LoginButton;
