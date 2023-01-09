fe9b2623f05e9766e08374a19ac1b3d200f8614a
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 23:26:06 -0300

    Add types and context to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CartContext } from './components';
 import Ecommerce from './pages/Ecommerce';
 
+interface CartItem {
+  product: {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+  },
+  quantity: number;
+}
 
 function App() {
-  return <Ecommerce/>
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      <Ecommerce cart={cart} setCart={setCart} />
+    </CartContext.Provider>
+  );
 }
 
 export default App;
