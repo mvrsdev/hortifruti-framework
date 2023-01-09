d711410b304792eb7b287773c445f8b6231811e3
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 23:36:01 -0300

    create index to pages folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as Checkout } from './Checkout';
+export { default as HomePage } from './HomePage';
+export { default as Login } from './Login';
+export { default as Cart } from './Cart';
