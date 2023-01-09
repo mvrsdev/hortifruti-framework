a12453c1323ccb5ebd56ee8186c6b2f988932257
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-09 14:14:48 -0300

    add LoginContext to Header comp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { LoginButton } from './index';
 import shoppingCart from '../assets/img/shopping-cart.png';
 import { useNavigate } from 'react-router-dom';
+import { LoginContext } from '../pages';
 
 const HeaderRow = styled.div`
   display: flex;
@@ -15,31 +16,36 @@ const HeaderRow = styled.div`
 `;
 
 const LogoContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-padding: 8px;
-cursor: pointer;
-h1 {
-  margin-left: 8px;
-}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  cursor: pointer;
+  h1 {
+    margin-left: 8px;
+  }
 `;
 
 const Header: React.FC = () => {
-  const navigate = useNavigate();
+  const { isLogged } = useContext(LoginContext);
 
+  const navigate = useNavigate();
   const loginClickHandler = () => {
     navigate('/login');
   };
 
   return (
-      <HeaderRow>
-        <LogoContainer>
-        <img src={shoppingCart} alt='Shopping Cart' width={26} height={26}/>
+    <HeaderRow>
+      <LogoContainer>
+        <img src={shoppingCart} alt="Shopping Cart" width={26} height={26} />
         <h1>Hortifruti Framework</h1>
-        </LogoContainer>
-      <LoginButton onClick={loginClickHandler}/>
-      </HeaderRow>
+      </LogoContainer>
+      {isLogged ? (
+        <div>Olá usuário</div>
+      ) : (
+        <LoginButton onClick={loginClickHandler} />
+      )}
+    </HeaderRow>
   );
 };
 
