08eb4c9a5de96c5259ea770dda047faf36c1a6c4
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2023-01-08 23:31:37 -0300

    change checkout comp from componets to pages

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,42 +1,37 @@
 import React from 'react';
-import { CartItem } from '../components/index';
+import styled from 'styled-components';
+import { CartItemProps } from '../components/CartContext';
+import { Button, CartItem } from '../components/index';
 
-interface CartItem {
-  product: {
-    name: string;
-    price: number;
-    image: string;
-  };
-  quantity: number;
-}
+const CheckoutWrapper = styled.div`
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 1px 16px 16px 16px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 75vw;
+  border: 1px solid #f0f0f0;
+  text-align: center;
+`;
 interface CheckoutProps {
-  items: CartItem[];
+  items: CartItemProps[];
 }
 
 const Checkout: React.FC<CheckoutProps> = ({ items }) => {
   return (
-    <div>
+    <CheckoutWrapper>
+      <h1>Shopping Cart</h1>
       {items.map((item) => (
         <CartItem
           key={item.product.name}
           product={item.product}
           quantity={item.quantity}
-          onRemove={() => null}
-        />
+          onRemove={() => null} id={0}        />
       ))}
-      <form>
-        <label>
-          Payment Method:
-          <input type="text" />
-        </label>
-        <br />
-        <label>
-          Shipping Address:
-          <input type="text" />
-        </label>
-      </form>
-      <button>Purchase</button>
-    </div>
+      <Button onClick={() => null}>Purchase</Button>
+    </CheckoutWrapper>
   );
 };
 
